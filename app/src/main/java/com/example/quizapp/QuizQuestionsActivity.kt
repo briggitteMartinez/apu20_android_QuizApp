83443f922330185f769e41e2da2be221c2ff7279
briggitteMartinez <briggitte90@hotmail.com>
2020-10-22 20:48:41 +0200

    changed the local variables to global variables

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -6,29 +6,35 @@ import android.util.Log
 import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    private var mCurrentPosition:Int = 1
+    private var mQuestionsList:ArrayList<Question>? = null
+    private var mSelectedOptionPosition : Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
-        val questionsList = Constants.getQuestions()
+         mQuestionsList = Constants.getQuestions()
 
-        // kolla storleken på frågorna
-        // Log.i("Questions Size", "${questionsList.size}")
+
+
 
         // plats på frågorna minus 1 pga index börjar på 0
-        val currentPosition = 1
-        val question: Question? = questionsList[currentPosition-1]
+    }
+    private fun setQuestions (){
+        mCurrentPosition = 1
+        val question= mQuestionsList!![mCurrentPosition-1]
 
-        progressBar.progress = currentPosition
-        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+        progressBar.progress = mCurrentPosition
+        tv_progress.text = "$mCurrentPosition" + "/" + progressBar.max
 
         tv_question.text = question!!.question
         iv_image.setImageResource(question.image)
         tv_opt_one.text = question.optOne
         tv_opt_two.text = question.optTwo
         tv_opt_three.text = question.optThree
-
-
 
     }
 }
