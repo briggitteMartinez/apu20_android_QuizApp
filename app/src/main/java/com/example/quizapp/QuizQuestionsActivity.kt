e6018c1d0773ca78d92d0d6feb7d10c6ea232654
briggitteMartinez <briggitte90@hotmail.com>
2020-10-22 21:16:36 +0200

    implement oncklicklistener to Quizquestions Activity.  set the selected options onclick and when they are not selected

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -1,11 +1,16 @@
 package com.example.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener{
 
     private var mCurrentPosition:Int = 1
     private var mQuestionsList:ArrayList<Question>? = null
@@ -18,6 +23,13 @@ class QuizQuestionsActivity : AppCompatActivity() {
 
          mQuestionsList = Constants.getQuestions()
 
+        setQuestions()
+
+        tv_opt_one.setOnClickListener(this)
+        tv_opt_two.setOnClickListener(this)
+        tv_opt_three.setOnClickListener(this)
+
+
 
 
 
@@ -26,6 +38,8 @@ class QuizQuestionsActivity : AppCompatActivity() {
     private fun setQuestions (){
         mCurrentPosition = 1
         val question= mQuestionsList!![mCurrentPosition-1]
+
+        defaultOptionsView()
 
         progressBar.progress = mCurrentPosition
         tv_progress.text = "$mCurrentPosition" + "/" + progressBar.max
@@ -36,5 +50,42 @@ class QuizQuestionsActivity : AppCompatActivity() {
         tv_opt_two.text = question.optTwo
         tv_opt_three.text = question.optThree
 
+    }
+
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>()
+        options.add(0, tv_opt_one)
+        options.add(1, tv_opt_two)
+        options.add(2, tv_opt_three)
+
+        for (option in options){
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable( this, R.drawable.default_option_border_bg)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.tv_opt_one -> {
+                selectedOptionView(tv_opt_one,1)
+            }
+            R.id.tv_opt_two -> {
+                selectedOptionView(tv_opt_two, 2)
+            }
+            R.id.tv_opt_three -> {
+                selectedOptionView(tv_opt_three, 3)
+            }
+
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int){
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable( this, R.drawable.select_option_border_bg)
     }
 }
