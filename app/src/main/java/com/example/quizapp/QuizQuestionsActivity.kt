53140e3c055d8fc2529c31ed5add3b002786d912
briggitteMartinez <briggitte90@hotmail.com>
2020-10-22 20:23:31 +0200

    find current position on questions, get them from object, find current position and maxposition to progressbar

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -3,6 +3,7 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,23 @@ class QuizQuestionsActivity : AppCompatActivity() {
 
         val questionsList = Constants.getQuestions()
 
-        // storleken på frågorna
+        // kolla storleken på frågorna
         // Log.i("Questions Size", "${questionsList.size}")
+
+        // plats på frågorna minus 1 pga index börjar på 0
+        val currentPosition = 1
+        val question: Question? = questionsList[currentPosition-1]
+
+        progressBar.progress = currentPosition
+        tv_progress.text = "$currentPosition" + "/" + progressBar.max
+
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_opt_one.text = question.optOne
+        tv_opt_two.text = question.optTwo
+        tv_opt_three.text = question.optThree
+
+
+
     }
 }
